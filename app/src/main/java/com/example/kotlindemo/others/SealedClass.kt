48aa9ab947a22beac736d4ee07965e6402432860
wangyingxi <wangyingxi@xiaomi.com>
2021-08-10 14:49:04 +0800

    延迟初始化和密封类

---
+++ app/src/main/java/com/example/kotlindemo/others/SealedClass.kt
@@ -0,0 +1,19 @@
+package com.example.kotlindemo.others
+
+import android.util.Log
+
+sealed interface SealedClass
+data class Const(val number: Double) : SealedClass
+data class Sum(val e1: SealedClass, val e2: SealedClass) : SealedClass
+object NotANumber : SealedClass
+
+fun eval(expr: SealedClass): Double = when(expr) {
+    is Const -> expr.number
+    is Sum -> eval(expr.e1) + eval(expr.e2)
+    NotANumber -> Double.NaN
+}
+
+fun main(args:Array<String>){
+    val const = Const(100.0)
+    println(eval(const))
+}
