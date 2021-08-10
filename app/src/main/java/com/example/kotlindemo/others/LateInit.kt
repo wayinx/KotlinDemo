ab061dfa09898d84bd8f90be1839a0c8129c0c95
wangyingxi <wangyingxi@xiaomi.com>
2021-08-10 14:49:04 +0800

    延迟初始化和密封类

---
+++ app/src/main/java/com/example/kotlindemo/others/LateInit.kt
@@ -0,0 +1,11 @@
+package com.example.kotlindemo.others
+
+class LateInit {
+    val name: String = ""
+    val age: Int = 0
+    lateinit var name1: String
+    //'lateinit' modifier is not allowed on properties of nullable types
+    //lateinit var name2 :String?
+    //'lateinit' modifier is not allowed on properties of primitive types
+    //lateinit var age1 :Int
+}
